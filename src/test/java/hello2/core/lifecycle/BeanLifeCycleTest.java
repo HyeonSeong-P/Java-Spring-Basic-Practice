9eaafe3a348e0bb6b4f70dbd03399b38e6f36e04
phs <gustjd0808@pusan.ac.kr>
2022-05-23 17:18:53 +0900

    빈 생명주기 콜백 - 빈 생명주기 콜백 시작, 인터페이스 InitializingBean, DisposableBean

---
+++ src/test/java/hello2/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package hello2.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+    @Test
+    public void lifeCycleTest(){
+        /** close를 위해 이전까지 쓰던 ApplicationContext 대신 하위 클래스를 사용해야 한다.**/
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
